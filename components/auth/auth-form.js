8e6e374a83d38f52882093f2d8ec8b6842eac0a8
Kaaeed <stoobideak4@gmail.com>
2024-01-17 20:18:05 +0100

    Added sign in functionality, front end functionality and functional api endpoint with mongodb

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,36 +1,155 @@
-import { useState } from 'react';
-import classes from './auth-form.module.css';
+import { useState, useRef, useEffect } from "react";
+import classes from "./auth-form.module.css";
+import Notification from "../../ui/notification";
+
+async function createUser(email, password) {
+  const response = await fetch("/api/auth/signup", {
+    method: "POST",
+    body: JSON.stringify({ email, password }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.message || "Something went wrong!");
+  }
+
+  return data;
+}
+
+const getNotificationObject = (requestStatus, requestError) => {
+  let notification;
+
+  switch (requestStatus) {
+    case "pending":
+      notification = {
+        status: "pending",
+        title: "Sending request...",
+        message: "Your request is on the way!",
+      };
+      break;
+    case "success":
+      notification = {
+        status: "success",
+        title: "Success!",
+        message: "Request sent successfully!",
+      };
+      break;
+    case "error":
+      notification = {
+        status: "error",
+        title: "Error!",
+        error: requestError,
+      };
+      break;
+  }
+
+  // if (requestStatus === ) {
+  //   notification = {
+  //     status: "pending",
+  //     title: "Sending request...",
+  //     message: "Your request is on the way!",
+  //   };
+  // } else if (requestStatus === "success"){
+  //   notification = {
+  //     status: "success",
+  //     title: "Success!",
+  //     message: "Request sent successfully!"
+  //   }
+  // } else if (requestStatus === "error"){
+  //   notification = {
+  //     status: "error",
+  //     title: "Error!",
+  //     error: requestError
+  //   }
+  // }
+
+  return notification;
+};
 
 function AuthForm() {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
+  const formRef = useRef();
+
   const [isLogin, setIsLogin] = useState(true);
+  const [requestStatus, setRequestStatus] = useState(); // "pending", "success", "error"
+  const [requestError, setRequestError] = useState();
+
+  useEffect(() => {
+    if (requestStatus === "success" || requestStatus === "error") {
+      const timer = setTimeout(() => {
+        setRequestStatus("");
+        setRequestError("");
+      }, 3000);
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [requestStatus]);
 
   function switchAuthModeHandler() {
     setIsLogin((prevState) => !prevState);
   }
 
+  const submitHandler = async (event) => {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredPassword = passwordInputRef.current.value;
+
+    // Possible to add client side validation
+
+    if (isLogin) {
+      // log user in
+    } else {
+      // create a new user
+      setRequestStatus("pending");
+      try {
+        const userData = await createUser(enteredEmail, enteredPassword);
+        setRequestStatus("success");
+        formRef.current.reset();
+      } catch (error) {
+        setRequestError(error.message);
+        setRequestStatus("error");
+      }
+    }
+  };
+
+  const notification = getNotificationObject(requestStatus, requestError);
+
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler} ref={formRef}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            ref={passwordInputRef}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
+      {notification && <Notification notification={notification} />}
     </section>
   );
 }
---
+++ pages/api/auth/signup.js
@@ -0,0 +1,50 @@
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  if (req.method !== "POST") {
+    return;
+  }
+
+  const data = req.body;
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({ message: "Invalid input - password" });
+    return;
+  }
+
+  let client;
+
+  try {
+    client = await connectToDatabase();
+  } catch (error) {
+    res.status(500).json({ message: "Could not connect to the database." });
+    return;
+  }
+
+  const db = client.db();
+
+  const hashedPassword = hashPassword(password);
+
+  let result;
+  try {
+    result = await db
+      .collection("users")
+      .insertOne({ email: email, password: hashedPassword });
+  } catch (error) {
+    client.close();
+    res.status(500).json({ message: "Storing to database failed!" });
+    return;
+  }
+
+  client.close();
+
+  res.status(201).json({ message: "Created an user!" });
+}
+export default handler;
